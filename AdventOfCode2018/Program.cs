215d82f77a4ece9efaafa0517c5da251a1786c52
vfridell <gusknows@gmail.com>
2018-12-24 13:52:41 -0500

    Thirteen

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode2018
     {
         static void Main(string[] args)
         {
-            Twelve.Part2();
+            Thirteen.Part2();
         }
     }
 }
--- AdventOfCode2018/Ten.cs
+++ AdventOfCode2018/Ten.cs
@@ -7,28 +7,7 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2018
 {
-    public class Vector : IRectangle
-    {
 
-        public Vector(Point position, Point velocity)
-        {
-            Position = position;
-            Velocity = velocity;
-            _rectangle = new Rect(Position.X - 1, Position.Y - 1, 3, 3);
-        }
-
-        public Point Position { get; set; }
-        public Point Velocity { get; set; }
-
-        private Rect _rectangle;
-        public Rect Rectangle => _rectangle;
-
-        public Vector GetVectorAt(int time)
-        {
-            Point newPosition = new Point(Position.X + (Velocity.X * time), Position.Y + (Velocity.Y * time));
-            return new Vector(newPosition, Velocity);
-        }
-    }
 
     public class Ten
     {
